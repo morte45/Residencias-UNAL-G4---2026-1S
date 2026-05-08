dbb041a49b14d47cb153b7e100e3699fee40d6b0
sebohorqueza <sebohorqueza@unal.edu.co>
2026-05-07 21:51:03 -0500

    Update Proyecto AVL V1.java

--- Proyecto AVL V1.java
+++ Proyecto AVL V1.java
@@ -1,8 +1,9 @@
 public class Bocetoproyecto{
   public static void main(String[] args){
   }
-
+  ///////////////////////////////////////////////////
   ///////////////Clase Estudiante////////////////////
+  ///////////////////////////////////////////////////
   class Estudiante implements Comparable<Estudiante>{
     private int id;
     private String nombre;
@@ -52,7 +53,83 @@ public class Bocetoproyecto{
         return "[ID: " + id + " Nombre: " + nombre + " Puntaje socioeconomico: " + puntajeSE + "]";
     }
   }
-
+  ////////////////////////////////////////////
   ///////////////Clase AVL////////////////////
-  
+  ////////////////////////////////////////////
+  class AVL<T extends Comparable<T>>{
+    private Node root;
+    
+    public Node getRoot() {
+        return root;
+    }
+    private int height(Node N) {
+        if (N == null)
+            return 0;
+        return N.height;
+    }
+    private int getBalance(Node N) {
+        if (N==null)
+            return 0;
+        return height(N.left)-height(N.right);
+    }
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
+    private Node leftRotate(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = Math.max(height(x.left), height(x.right)) + 1;
+        y.height = Math.max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+    public void insert(T data) {
+        root = insertRec(root, data);
+    }
+    private Node insertRec(Node node, T data) {
+        if(node==null){
+            return (new Node(data));
+        }
+        int cmp = data.compareTo((T)node.data);
+        if(cmp < 0){
+            node.left = insertRec(node.left, data);
+        }
+        else if (cmp > 0) {
+            node.right = insertRec(node.right, data);
+        }
+        else {
+            return node;
+        }
+        node.height=1+Math.max(height(node.left),height(node.right));
+        int balance=getBalance(node);
+        if(balance>1 && data.compareTo((T)node.left.data)<0){
+            return rightRotate(node);
+        }
+        if(balance<-1 && data.compareTo((T)node.right.data)>0){
+            return leftRotate(node);
+        }
+        if(balance>1 && data.compareTo((T)node.left.data)>0){
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+        if(balance<-1 && data.compareTo((T)node.right.data)<0){
+            node.right=rightRotate(node.right);
+            return leftRotate(node);
+        }
+        return node;
+    }
+  }
 }
